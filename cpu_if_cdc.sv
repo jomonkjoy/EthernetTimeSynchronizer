eb406092168b6f35265edb21ccf2f472958cc968
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-02-20 18:44:59 +0000

    Update cpu_if_cdc.sv

--- cpu_if_cdc.sv
+++ cpu_if_cdc.sv
@@ -91,3 +91,59 @@ module cpu_if_cdc #(
   assign l_cpu_if_read_data = cpu_if_read_data;
   
 endmodule
+// Data-Sync : synchronize single-bit data
+// Min 3 stage pipeline to mitegate metastability due to setup and hold time violations
+// use ASYNC_REG and max_delay[with min-period(freq1,freq2)] constraint with async-clock groups {clk1,clk2}
+module data_sync #(
+  parameter SYNC_STAGE = 3
+) (
+  input  logic clk,
+  input  logic din,
+  output logic dout
+);
+  
+  (* ASYNC_REG = "TRUE" *) logic [SYNC_STAGE-1:0] sync_reg;
+  always_ff @(posedge clk) begin
+    sync_reg <= {sync_reg[SYNC_STAGE-2:0],din};
+  end
+  assign dout = sync_reg[SYNC_STAGE-1];
+  
+endmodule
+// Data-Sync-pulsegen : synchronize singl-pulse from source_clk to destination_clk
+module data_sync_pulsegen #(
+  parameter SYNC_STAGE = 3
+) (
+  input  logic aclk,
+  input  logic areset,
+  input  logic adin, // pulse
+  output logic aqualifier,
+  input  logic bclk,
+  output logic bdout,// pulse
+  output logic bqualifier
+);
+  
+  logic bqualifier_d;
+  // qualifier signal generation using T-FF
+  always_ff @(posedge aclk) begin
+    if (areset) begin
+      aqualifier <= 1'b0;
+    end else begin
+      aqualifier <= aqualifier ^ adin;
+    end
+  end
+  // synchronize qualifier from aclk to bclk
+  // 3-clock-cycle for a double-flop to get qualifier valid in bclk-domain
+  data_sync #(
+    .SYNC_STAGE (SYNC_STAGE)
+  ) data_sync_qualifier (
+    .clk  (bclk),
+    .din  (aqualifier),
+    .dout (bqualifier)
+  );
+  // pulse generation for each logic transition
+  always_ff @(posedge aclk) begin
+    bqualifier_d <= bqualifier;
+  end
+  assign bdout = bqualifier_d ^ bqualifier;
+  
+endmodule
